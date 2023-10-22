74b87e7447c3f74c7f50e38e9c3cb7c0ad14bc9f
Christophe Peugnet <christophe.peugnet@outlook.fr>
2023-10-22 12:18:12 +0200

    Creation of the project

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Blazor_Revolut_Card_Payment.Components;
+
+namespace Blazor_Revolut_Card_Payment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
